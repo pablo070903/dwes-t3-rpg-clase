9a012710d24870a8fd5a0dfd70b7020b4c5d8014
pablo070903 <151624007+pablo070903@users.noreply.github.com>
2024-11-26 10:47:29 +0100

    creo el create_item

---
+++ src/views/partials/_menu.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/views/jugar.php">Jugar</a></li>
+        <li><a href="/views/characters/create_character.php">Personajes</a></li>
+        <li><a href="/views/create_enemy.php">Enemigos</a></li>
+        <li><a href="/views/create_map.php">Mapas</a></li>
+    </ul>
+</nav>
